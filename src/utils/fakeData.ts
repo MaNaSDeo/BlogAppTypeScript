79c2d50ab28be446dd630053a783d788a0bd7b37
manasdeo <rmnslogin@gmail.com>
2024-10-13 11:31:48 +0530

    fake data

---
+++ src/utils/fakeData.ts
@@ -0,0 +1,20 @@
+import { BlogPost } from "../types";
+
+export const fakeBlogPosts: BlogPost[] = [
+  {
+    id: "1",
+    title: "My First Blog Post",
+    content: "This is the content of my first blog post.",
+    author: "John Doe",
+    date: "2024-10-13",
+    location: "US",
+  },
+  {
+    id: "2",
+    title: "Traveling in Europe",
+    content: "My experiences traveling through Europe...",
+    author: "Jane Smith",
+    date: "2024-10-12",
+    location: "FR",
+  },
+];
