1a782c6d26e706657a85945302d586030581f05c
manasdeo <rmnslogin@gmail.com>
2024-10-13 11:35:51 +0530

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div>App</div>;
+import React from "react";
+import BlogFeed from "./components/BlogFeed";
+import BlogEditor from "./components/BlogEditor";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>My Blog App</h1>
+      <BlogFeed />
+      <BlogEditor />
+    </div>
+  );
 };
 
 export default App;
